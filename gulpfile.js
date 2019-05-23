e380ebe80a3c6544773a50566318535f1b554591
Петрицкий Альберт <quakush@mail.ru>
2019-05-23 13:56:26 +0700

    Автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,16 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var svgstore = require("gulp-svgstore");
+var del = require("del");
+
+gulp.task("clean", function () {
+  return del("build");
+ });
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -16,14 +26,15 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"));
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -35,3 +46,50 @@ gulp.task("server", function () {
 });
 
 gulp.task("start", gulp.series("css", "server"));
+
+gulp.task("sprite", function() {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({ inlineSvg: true}))
+    .pipe(rename("sprites.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("opti-img", function() {
+  return gulp.src("source/img/*.{png,ipg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 1}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/*.{png,jpg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("source/img"));
+ });
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "source/fonts/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
+    ], {
+    base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+  .pipe(gulp.dest("build/"));
+ });
+
+gulp.task("build", gulp.series(
+  "clean",
+  "copy",
+  "css",
+  "html"
+  ));
